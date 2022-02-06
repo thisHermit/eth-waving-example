44e8cbc48ddc86ae7650aa711503688f819d9a29
Bashar Jaan Khan <basharjaankhan@gmail.com>
2022-02-06 09:39:48 +0000

    Add Wave retrieval on wallet connection

--- src/App.jsx
+++ src/App.jsx
@@ -69,11 +69,10 @@ export default function App() {
         alert("Get Metamask!");
         return;
       }
-
       const accounts = await ethereum.request({ method: "eth_requestAccounts" });
-
-      console.log("Connected", accounts[0])
+      console.log("Connected!", accounts[0])
       setCurrentAccount(accounts[0])
+      getAllWaves()
     } catch (error) {
       console.log(error)
     }
